c21b4634dc3e0dadaa6e5df5c46316d03e40adb5
David <dave.leslie.online@gmail.com>
2016-09-29 16:13:07 +0200

    updated styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,14 @@ function adjust_textarea(h) {
     h.style.height = "20px";
     h.style.height = (h.scrollHeight)+"px";
 }
+
+$(document).ready(function () {
+   $(".services-logo").hide().each(function() {
+       $(".s1").delay(1000).fadeIn(1000);
+       $(".s2").delay(1200).fadeIn(1000);
+       $(".s3").delay(1400).fadeIn(1000);
+       $(".s4").delay(1600).fadeIn(1000);
+   });
+});
+
+
